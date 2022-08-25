b010d41d6d4d4a36146bd7faefe1eb30e5755863
Denis <chekulaev555@gmail.com>
2022-08-25 21:53:43 +0300

    add final work

--- src/test/java/guru/qa/SelenideStepsTest.java
+++ src/test/java/guru/qa/SelenideStepsTest.java
@@ -2,7 +2,9 @@ package guru.qa;
 
 import com.codeborne.selenide.Condition;
 import com.codeborne.selenide.logevents.SelenideLogger;
+import io.qameta.allure.*;
 import io.qameta.allure.selenide.AllureSelenide;
+import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
 import org.openqa.selenium.By;
 
@@ -13,29 +15,47 @@ import static io.qameta.allure.Allure.step;
 import static org.openqa.selenium.By.linkText;
 
 public class SelenideStepsTest extends TestBase {
-   private static final String repository = "eroshenkoam/allure-example";
-   private static final String nameIssue = "С Новым Годом (2022)";
+    private static final String repository = "eroshenkoam/allure-example";
+    private static final String nameIssue = "С Новым Годом (2022)";
 
     @Test
     public void testLambdaStep() {
         SelenideLogger.addListener("allure", new AllureSelenide());
 
-        step("Открываем главную страницу GitHub",() -> {
+        step("Открываем главную страницу GitHub", () -> {
             open("https://github.com");
         });
-        step("Ищем репозиторий" + repository,() ->{
+        step("Ищем репозиторий" + repository, () -> {
             $(".header-search-input").click();
             $(".header-search-input").setValue(repository);
             $(".header-search-input").pressEnter();
         });
-        step("Кликаем по ссылке репозитория"  + repository,() -> {
+        step("Кликаем по ссылке репозитория" + repository, () -> {
             $(By.linkText(repository)).click();
         });
-        step("Открываем таб Issues",() ->{
+        step("Открываем таб Issues", () -> {
             $("#issues-tab").click();
         });
-        step("Проверяем наличие Issue с названием" + nameIssue,() -> {
+        step("Проверяем наличие Issue с названием" + nameIssue, () -> {
             $(withText(nameIssue)).should(Condition.exist);
         });
+    }
+
+    @Test
+    @Feature("Issue в репозитории")
+    @Story("Создание Issue")
+    @Owner("chekulaevda")
+    @Severity(SeverityLevel.CRITICAL)
+    @DisplayName("Создание Issue пользователем")
+    public void testAnnotatedStep() {
+        SelenideLogger.addListener("allure", new AllureSelenide());
+        WebSteps steps = new WebSteps();
+
+        steps.openMainPage();
+        steps.searchForRepository(repository);
+        steps.clickOnRepository(repository);
+        steps.openIssueTab();
+        steps.searchNameIssue(nameIssue);
+
     }
 }
